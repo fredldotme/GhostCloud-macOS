8cdcf5e9ccf9759a7e9070709be47554da480bfa
Alfred Neumayer <dev.beidl@gmail.com>
2021-06-17 04:13:59 +0200

    Initial Commit

---
+++ GhostCloud-macOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GhostCloud-macOS
+//
+//  Created by Alfred Neumayer on 17.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GhostCloud-macOS/GhostCloud_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostCloud_macOSApp.swift
+//  GhostCloud-macOS
+//
+//  Created by Alfred Neumayer on 17.06.21.
+//
+
+import SwiftUI
+
+@main
+struct GhostCloud_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
